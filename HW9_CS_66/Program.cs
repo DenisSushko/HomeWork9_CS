713dca4c5535b93d8b0efbaefab746e979494a85
Denis <help@denis-getcourse.ru>
2022-11-27 18:58:28 +0300

    HW9_CS_66

---
+++ HW9_CS_66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+void sumRec(int M, int N, int sum)
+{
+    if (N < 1 || M < 1)
+    {
+        return;
+    }
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return ;
+    }
+    else
+    {
+        sum = sum + (M++);
+        sumRec(M, N, sum);
+    }
+
+}
+
+Console.WriteLine("Введите число M");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+sumRec(M, N, 0);
